91960f5864931f37fd738ff766e6c1edaac1e9de
ttoy12 <thomastoy1415@gmail.com>
2024-08-28 16:57:49 -0700

    contact section prefills google form
    Should change so it doesn't need to go through google form.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -125,7 +125,7 @@ export default function Home() {
 
             <h3 className="text-xl font-bold mb-4">Skills</h3>
             <div className="flex flex-wrap justify-center mb-8">
-              {['C++', 'C', 'Python', 'Node.js', 'JavaScript', 'React', 'Nextjs', 'Expo', 'React Native', 'TypeScript', 'Jest', 'HTML', 'CSS', 'SQL'].map(skill => (
+              {['C++', 'C', 'Python', 'Node.js', 'JavaScript', 'React', 'Nextjs', 'Expo', 'React Native', 'TypeScript', 'Vue.js', 'Jest', 'HTML', 'CSS', 'SQL'].map(skill => (
                 <span key={skill} className="bg-gray-800 text-white py-2 px-4 m-2 rounded-lg">
                   {skill}
                 </span>
@@ -205,18 +205,23 @@ export default function Home() {
           <div className="container mx-auto text-center">
             <h2 className="text-3xl font-bold mb-8">Contact Me</h2>
             <p className="text-lg mb-4">Feel free to get in touch with me through the form below:</p>
-            <form className="max-w-lg mx-auto">
+            <form 
+              className="max-w-lg mx-auto"
+              action="https://docs.google.com/forms/u/2/d/14LxY3O-IcrsCyobt81pzNw3DHpM2u-609Yh2cmbqIks/prefill"
+              method="POST"
+              target="_blank"
+            >
               <div className="mb-4">
                 <label htmlFor="name" className="block text-sm font-medium mb-2">Name</label>
-                <input type="text" id="name" className="w-full px-4 py-2 border rounded-lg text-black" required />
+                <input type="text" id="name" className="w-full px-4 py-2 border rounded-lg text-black" name="entry.1913529574" required />
               </div>
               <div className="mb-4">
                 <label htmlFor="email" className="block text-sm font-medium mb-2">Email</label>
-                <input type="email" id="email" className="w-full px-4 py-2 border rounded-lg text-black" required />
+                <input type="email" id="email" className="w-full px-4 py-2 border rounded-lg text-black" name="entry.498883965" required />
               </div>
               <div className="mb-4">
                 <label htmlFor="message" className="block text-sm font-medium mb-2">Message</label>
-                <textarea id="message" rows={4} className="w-full px-4 py-2 border rounded-lg text-black" required></textarea>
+                <textarea id="message" rows={4} className="w-full px-4 py-2 border rounded-lg text-black" name="entry.811883332" required></textarea>
               </div>
               <button type="submit" className="bg-blue-800 text-white px-6 py-3 rounded-lg">Send Message</button>
             </form>
