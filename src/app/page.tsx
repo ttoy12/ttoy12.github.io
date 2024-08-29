d39724c72fb3e496a4efb33e36f38cb240188f4b
ttoy12 <thomastoy1415@gmail.com>
2024-08-28 17:23:15 -0700

    header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import DetectiveDollarSlideShow from './components/DetectiveDollarSlideshow';
+import Header from './components/Header';
 
 
 export default function Home() {
@@ -44,74 +45,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="bg-blue-800 text-white p-6 fixed top-0 left-0 w-full z-10">
-        <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-3xl font-bold">My Portfolio</h1>
-          <button
-            className="text-white md:hidden focus:outline-none"
-            onClick={toggleMenu}
-          >
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d={menuOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}
-              ></path>
-            </svg>
-          </button>
-          <nav
-            className={`${
-              menuOpen ? 'block' : 'hidden'
-            } md:flex space-x-4 absolute md:static w-full md:w-auto left-0 top-full md:top-auto`}
-          >
-            <ul className="flex flex-col md:flex-row space-y-4 bg-blue-800 md:space-y-0 md:space-x-4 p-4 md:p-0">
-              <li className="text-xl">
-                <Link
-                  href="#about"
-                  className="px-3 py-2 border-b-2 border-transparent hover:border-yellow-400 transition-all duration-500"
-                  onClick={() => setMenuOpen(false)}
-                >
-                  About
-                </Link>
-              </li>
-              <li className="text-xl">
-                <Link
-                  href="#experience"
-                  className="px-3 py-2 border-b-2 border-transparent hover:border-yellow-400 transition-all duration-500"
-                  onClick={() => setMenuOpen(false)}
-                >
-                  Experience
-                </Link>
-              </li>
-              <li className="text-xl">
-                <Link
-                  href="#projects"
-                  className="px-3 py-2 border-b-2 border-transparent hover:border-yellow-400 transition-all duration-500"
-                  onClick={() => setMenuOpen(false)}
-                >
-                  Projects
-                </Link>
-              </li>
-              <li className="text-xl">
-                <Link
-                  href="#contact"
-                  className="px-3 py-2 border-b-2 border-transparent hover:border-yellow-400 transition-all duration-500"
-                  onClick={() => setMenuOpen(false)}
-                >
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
+      <Header menuOpen={menuOpen} toggleMenu={toggleMenu} />
 
       <main>
         <section id="about" className="hero bg-gray-900 text-white py-20">
