4c0d0d6424d75ad224e91a821ed4d41f7af79883
ttoy12 <thomastoy1415@gmail.com>
2024-08-29 15:40:58 -0700

    progress on emailjs
    need to figure out how to get the email and name to be displayed

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,34 +1,54 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef<HTMLFormElement>(null);
+
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_wx9rt9o', 'template_br9qfev', form.current, {
+        publicKey: 'KmrpDOrH6czSND86J',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+    e.target.reset();
+  };
+
   return (
     <section id="contact" className="bg-gray-900 text-white py-20">
-    <div className="container mx-auto text-center">
-      <h2 className="text-3xl font-bold mb-8">Contact Me</h2>
-      <p className="text-lg mb-4">Feel free to get in touch with me through the form below:</p>
-      <form 
-        className="max-w-lg mx-auto"
-        action="https://docs.google.com/forms/u/2/d/14LxY3O-IcrsCyobt81pzNw3DHpM2u-609Yh2cmbqIks/prefill"
-        method="POST"
-        target="_blank"
-      >
-        <div className="mb-4">
-          <label htmlFor="name" className="block text-sm font-medium mb-2">Name</label>
-          <input type="text" id="name" className="w-full px-4 py-2 border rounded-lg text-black" name="entry.1913529574" required />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-sm font-medium mb-2">Email</label>
-          <input type="email" id="email" className="w-full px-4 py-2 border rounded-lg text-black" name="entry.498883965" required />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="message" className="block text-sm font-medium mb-2">Message</label>
-          <textarea id="message" rows={4} className="w-full px-4 py-2 border rounded-lg text-black" name="entry.811883332" required></textarea>
-        </div>
-        <button type="submit" className="bg-blue-800 text-white px-6 py-3 rounded-lg">Send Message</button>
-      </form>
-    </div>
-  </section>
-  )
-}
+      <div className="container mx-auto text-center">
+        <h2 className="text-3xl font-bold mb-8">Contact Me</h2>
+        <p className="text-lg mb-4">Feel free to get in touch with me through the form below:</p>
+        <form 
+          ref={form}
+          className="max-w-lg mx-auto"
+          onSubmit={sendEmail}
+        >
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-sm font-medium mb-2">Name</label>
+            <input type="text" id="name" className="w-full px-4 py-2 border rounded-lg text-black" name="user_name" required />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-sm font-medium mb-2">Email</label>
+            <input type="email" id="email" className="w-full px-4 py-2 border rounded-lg text-black" name="user_email" required />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="message" className="block text-sm font-medium mb-2">Message</label>
+            <textarea id="message" rows={4} className="w-full px-4 py-2 border rounded-lg text-black" name="message" required></textarea>
+          </div>
+          <button type="submit" className="bg-blue-800 text-white px-6 py-3 rounded-lg">Send Message</button>
+        </form>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
