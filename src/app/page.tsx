565615456099c592eadba735326449d690e9d0c5
Thomas Toy <thomastoy1415@gmail.com>
2024-08-30 02:43:26 -0700

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 
 export default function Home() {
@@ -30,6 +31,7 @@ export default function Home() {
         <Projects />
         <Contact />
       </main>
+      <Footer />
       
       <Script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12" strategy="beforeInteractive" />
     </div>
