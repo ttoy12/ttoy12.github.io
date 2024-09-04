885171db0440154b1a3bb1361754805ef54e19a4
Thomas Toy <thomastoy1415@gmail.com>
2024-09-03 20:40:35 -0700

    change button hover colors

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-4">
-    <div className="container mx-auto text-center flex flex-col items-center">
+      <div className="container mx-auto text-center flex flex-col items-center">
         <div className="flex space-x-6">
           <a href="mailto:thomastoy1415@gmail.com" className="text-white hover:text-gray-400 transition-colors duration-300" aria-label="Email">
-            <FontAwesomeIcon icon={faEnvelope} className="w-6 h-6 text-white hover:text-blue-500 transition-colors duration-300" />
+            <FontAwesomeIcon icon={faEnvelope} className="w-6 h-6 text-white hover:text-blue-600 transition-colors duration-300" />
           </a>
           <a href="https://www.linkedin.com/in/thomas-toy/" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faLinkedin} className="w-6 h-6 text-white hover:text-blue-500 transition-colors duration-300" />
+            <FontAwesomeIcon icon={faLinkedin} className="w-6 h-6 text-white hover:text-blue-600 transition-colors duration-300" />
           </a>
           <a href="https://github.com/ttoy12" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faGithub} className="w-6 h-6 text-white hover:text-blue-500 transition-colors duration-300" />
+            <FontAwesomeIcon icon={faGithub} className="w-6 h-6 text-white hover:text-blue-600 transition-colors duration-300" />
           </a>
         </div>
       </div>
-  </footer>
+    </footer>
   )
 }
 
